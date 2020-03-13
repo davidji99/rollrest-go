576011503277f08d7c771602e1c73e10393e1876
DJ <davidji99@users.noreply.github.com>
2020-03-13 20:05:55 +0900

    add simple client test [#5]

--- test/client_test.go
+++ test/client_test.go
@@ -1,40 +1,13 @@
 package test
 
 import (
-	"fmt"
-	"github.com/davidji99/rollbar-go/rollbar"
-	"io/ioutil"
-	"net/http"
-	"net/http/httptest"
+	"github.com/stretchr/testify/assert"
+	"testing"
 )
 
-var (
-	mux       *http.ServeMux
-	server    *httptest.Server
-	client    *rollbar.Client
-	clientErr error
-)
+func TestNewClient(t *testing.T) {
+	teardown := setup()
+	defer teardown()
 
-func setup() func() {
-	mux = http.NewServeMux()
-	server = httptest.NewServer(mux)
-
-	fmt.Println(server.URL)
-	client, clientErr = rollbar.New(rollbar.BaseURL(server.URL), rollbar.AuthAAT("account_access_token"),
-		rollbar.AuthPAT("project_access_token"))
-	if clientErr != nil {
-		panic(clientErr)
-	}
-
-	return func() {
-		server.Close()
-	}
-}
-
-func fixture(path string) string {
-	b, err := ioutil.ReadFile("../testdata/fixtures/" + path)
-	if err != nil {
-		panic(err)
-	}
-	return string(b)
+	assert.NotNil(t, client)
 }
